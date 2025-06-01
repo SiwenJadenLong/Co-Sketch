dd85248df98eab1b2e6e3794deb5e6495f7b14b7
Jadenlong <jadenlong376@gmail.com>
2025-05-31 19:24:51 -0700

    Bug fix with being able to jump midair

--- scenes/objects/player_1.gd
+++ scenes/objects/player_1.gd
@@ -1,19 +1,21 @@
 extends CharacterBody2D
 
-const Acceleration = 50
-const JUMP_ACCELERATION = -50
 var canjump = true
+
+
 @onready var jumptimer = $Timer
 
+#Player movement variables
+@export var Xacceleration : float = 50
 @export var Jumptime : float = 0.1
-@export var Jumpspeed : int = 90
+@export var Jumpspeed : int = 80
 @export var Jumpspeedcap : int = -500
 @export var Camera_Speed : int = 1
 @export var Gound_Reistance : int = 40
-@export var Air_Reistance : int = 10
+@export var Air_Reistance : int = 15
+
 
 func _physics_process(delta):
-
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
@@ -25,25 +27,22 @@ func _physics_process(delta):
 			velocity.y -= Jumpspeed
 		else:
 			velocity.y = Jumpspeedcap
-		
+	elif Input.is_action_just_released("ui_up"):
+		jumptimer.stop()
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
 		if velocity.x <= 200 and velocity.x >= -200:
-			velocity.x += direction * Acceleration
+			velocity.x += direction * Xacceleration
 		else:
 			velocity.x = direction * 200
 	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, 40)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 10)
-	
+
 	#$Camera2D.position_smoothing_speed = 7 + velocity.length() * CameraSpeedmultiplier
 	#print($Camera2D.position_smoothing_speed)
 	
 	move_and_slide()
-
-
-func _on_timer_timeout():
-	canjump = false
