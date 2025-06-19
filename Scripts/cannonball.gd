ad8b1ed4307a748f61fa2137ba191e153b015e1b
Jadenlong <jadenlong376@gmail.com>
2025-06-19 12:52:43 -0700

    pause menu working

--- Scripts/cannonball.gd
+++ Scripts/cannonball.gd
@@ -7,12 +7,10 @@ var directionrotated: float
 var spawnpos: Vector2
 
 func _ready():
-	#global_position = spawnpos
 	speed = speed.rotated(directionrotated)
 
 func _physics_process(delta):
 	position += speed
 
-func _on_area_entered(area):
-	#get_node()
-	pass
+func _left_screen():
+	queue_free()
--- scenes/Gameplay/main.gd
+++ scenes/Gameplay/main.gd
@@ -2,15 +2,27 @@ extends Node
 
 @export var currentlevel : PackedScene
 var paused = false
+@onready var main_ui = $"Main UI"
+@onready var pause_menu = $"Main UI/Pause Menu"
+@onready var level = $Level
+
+
+
+func _process(delta):
+	print(paused)
+
+
 func _ready():
 	loadnewlevel()
-	Signalbus.playerdeath.connect(transition);
+	Signalbus.playerdeath.connect(playerdeath);
 	
-func transition():
+func playerdeath():
+	
 	$AnimationPlayer.play("generictransition ahh");
 
 func loadnewlevel():
-	$Level.add_child(currentlevel.instance())
+	#$Level.add_child(currentlevel.instance())
+	pass
 
 func _input(event):
 #	Pause Menu
@@ -21,12 +33,27 @@ func _input(event):
 func togglepause():
 	paused = !paused
 	if paused:
-		$Level.process_mode = Node.PROCESS_MODE_DISABLED
+		$Level.process_mode = Node.PROCESS_MODE_DISABLED;
+		pause_menu.visible = true;
+		get_tree().paused = true;
 	elif !paused:
 		$Level.process_mode = Node.PROCESS_MODE_INHERIT
+		pause_menu.visible = false;
+		get_tree().paused = false;
 	else:
 		print("how did you get here?")
 
-
+#Pause Menu Code-----------------------------------
 func on_resume_button_pressed():
 	togglepause()
+	
+func _on_restart_pressed():
+	pass # Replace with function body.
+	
+func _on_main_menu_pressed():
+	pass # Replace with function body.
+
+func _on_level_select_pressed():
+	pass # Replace with function body.
+
+#-----------------------------------
