a7b14cbfde418285da190934b2c2feade44a4c87
Jadenlong <jadenlong376@gmail.com>
2025-06-18 23:51:29 -0700

    pause menu and level switching started

--- scenes/Gameplay/main.gd
+++ scenes/Gameplay/main.gd
@@ -1,7 +1,7 @@
 extends Node
 
-@export var currentlevel : Node2D
-
+@export var currentlevel : PackedScene
+var paused = false
 func _ready():
 	loadnewlevel()
 	Signalbus.playerdeath.connect(transition);
@@ -10,4 +10,23 @@ func transition():
 	$AnimationPlayer.play("generictransition ahh");
 
 func loadnewlevel():
-	$Level.add_child(currentlevel)
+	$Level.add_child(currentlevel.instance())
+
+func _input(event):
+#	Pause Menu
+	if event.is_action("Pause"):
+		togglepause()
+
+#toggling pause, freezes Level node
+func togglepause():
+	paused = !paused
+	if paused:
+		$Level.process_mode = Node.PROCESS_MODE_DISABLED
+	elif !paused:
+		$Level.process_mode = Node.PROCESS_MODE_INHERIT
+	else:
+		print("how did you get here?")
+
+
+func on_resume_button_pressed():
+	togglepause()
