770fd70793f94f4cf98ea3ed1be8dc3d72660d4b
Kayla Blosser <kayla.blosser@proton.me>
2025-06-23 22:48:22 -0400

    Attempt to create basic line drawing

--- scenes/Gameplay/cursor.gd
+++ scenes/Gameplay/cursor.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+# @onready var parent = $".";
+@export var lineTemplate: PackedScene = preload("res://scenes/Gameplay/linemaker/lineTemplate.tscn");
+
+@onready var line := lineTemplate.instantiate();
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,3 +12,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = get_global_mouse_position();
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("drawEvent"):
+		addLinePoint(position);
+		
+func addLinePoint(mousePosition: Vector2) -> void:
+	line.get_node("Line2D").add_point(mousePosition);
