d6a9c15868e65dde028255688f49f6821848964e
Jadenlong <jadenlong376@gmail.com>
2025-06-23 21:35:45 -0700

    Script cleanup

--- Scripts/winScreen.gd
+++ Scripts/winScreen.gd
@@ -1,1 +1,22 @@
 extends Control
+
+@onready var finalTime: Label = $Background/Panel/MarginContainer/VBoxContainer/finalTime
+
+func _ready():
+	SignalBus.levelWon.connect(_levelWon)
+
+func _process(delta):
+	finalTime.text = "Finished in: %s" % str(GlobalVariables.levelTime).pad_decimals(2);
+
+func _levelWon():
+	show()
+
+
+func winScreenLevelSelectPressed() -> void:
+	get_parent().get_node("levelSelect").show()
+
+
+func winScreenNextLevelPressed() -> void:
+	var nextLevelNumber = GlobalVariables.currentLevelNumber + 1
+	SignalBus.levelChange.emit(nextLevelNumber)
+	hide()
