7858ed6d8ea01d0837807a0eab534cfdbb2620b2
Kayla Blosser <kayla.blosser@proton.me>
2025-06-26 02:54:32 -0400

    Add player deaths to level winscreen

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -56,8 +56,6 @@ func loadNewLevel(levelnumber : int):
 	stats.show();
 	hideallpopupui();
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN);
-	GlobalVariables.perLevelOrangeDeathCounter = 0;
-	GlobalVariables.perLevelBlueDeathCounter = 0;
 #----------------------------------------------------------------------
 
 # Pausing-----------------------------------
--- Scripts/winScreen.gd
+++ Scripts/winScreen.gd
@@ -1,15 +1,20 @@
 extends Control
 
 @onready var finalTime: Label = $Background/Panel/MarginContainer/VBoxContainer/finalTime
+@onready var player1Deaths: Label = $Background/Panel/MarginContainer/VBoxContainer/HBoxContainer2/P1/perLevelDeaths
+@onready var player2Deaths: Label = $Background/Panel/MarginContainer/VBoxContainer/HBoxContainer2/P2/perLevelDeaths
 
 func _ready():
 	SignalBus.levelWon.connect(_levelWon)
 
 func _process(delta):
-	finalTime.text = "Finished in: %s" % str(GlobalVariables.levelTime).pad_decimals(2);
-
+	pass
 func _levelWon():
-	show()
+	finalTime.text = "Finished in: %s" % str(GlobalVariables.levelTime).pad_decimals(2);
+	player1Deaths.text = "Deaths: %s" % GlobalVariables.perLevelOrangeDeathCounter;
+	player2Deaths.text = "Deaths: %s" % GlobalVariables.perLevelBlueDeathCounter;
+	
+	show();
 
 #TODO Add Death Counter per player on win screen
 # Characters will stand on opposite sides and squint at each other while the death counter counts up.
@@ -23,3 +28,6 @@ func winScreenNextLevelPressed() -> void:
 	var nextLevelNumber = GlobalVariables.currentLevelNumber + 1
 	SignalBus.levelChange.emit(nextLevelNumber)
 	hide()
+	
+	GlobalVariables.perLevelOrangeDeathCounter = 0;
+	GlobalVariables.perLevelBlueDeathCounter = 0;
