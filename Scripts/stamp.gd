9a57d957646c602ad9aa1af38e72111fe0578960
Jadenlong <jadenlong376@gmail.com>
2025-06-27 16:20:01 -0700

    speed adjustments with stamps and staple remover fix

--- Scripts/stamp.gd
+++ Scripts/stamp.gd
@@ -2,8 +2,10 @@ extends Area2D;
 
 @export var stomping : bool;
 @export var offsetDelay : float = 0;
+@export var speedMultiplier : float = 1;
 
 func _ready() -> void:
+	$AnimationPlayer.speed_scale = speedMultiplier;
 	await get_tree().create_timer(offsetDelay).timeout
 	if stomping:
 		$AnimationPlayer.play("stomp");
