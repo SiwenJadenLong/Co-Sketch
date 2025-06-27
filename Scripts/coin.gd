4c8b3e60742a4123d53f4310fccb8e9115902e64
Kayla Blosser <kayla.blosser@proton.me>
2025-06-27 19:16:45 -0400

    Add penny to coin

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,29 +1,19 @@
 extends Area2D
 class_name coin
-@export var amplitude: int;
-@export var bobSpeed: float;
+
+@export var penny: bool = false;
+
 var up: bool = true;
 
 var preTween: float;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	if penny:
+		$Sprite2D.animation = "penny";
+	else:
+		$Sprite2D.animation = "default";
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#TODO Tween this value
-	
-	if up:
-		if preTween <= amplitude:
-			preTween += bobSpeed;
-		else:
-			up = false;
-	else:
-		if preTween >= -amplitude:
-			preTween -= bobSpeed;
-		else:
-			up = true;
-			
-	position.y = preTween;
+	pass
