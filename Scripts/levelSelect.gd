6ba7b455b3a5740447a195c80b194ac652b7c8e0
Jadenlong <jadenlong376@gmail.com>
2025-06-27 01:02:05 -0700

    PaperShredder and script cleanup

--- Scripts/levelSelect.gd
+++ Scripts/levelSelect.gd
@@ -1,4 +1,17 @@
 extends Control
 
+@onready var levelGrid: GridContainer = $"Level Grid";
+const levelButton = preload("res://scenes/UI/level_button.tscn");
+
 func levelSelectExitPressed():
-	hide()
+	hide();
+
+func _ready() -> void:
+	var numberOfLevels : int = 0;
+	var dir := DirAccess.open("res://scenes/levels");
+	if dir == null: printerr("Could not open folder"); return;
+	for file in dir.get_files():
+		numberOfLevels += 1;
+		var newlevelbutton = levelButton.instantiate();
+		newlevelbutton.levelNumber = numberOfLevels;
+		levelGrid.add_child(newlevelbutton);
