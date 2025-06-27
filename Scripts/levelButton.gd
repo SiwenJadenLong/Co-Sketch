18205af513a35669293d1e06fd185ea62069e0f7
Jadenlong <jadenlong376@gmail.com>
2025-06-27 15:33:16 -0700

    level select revamped

--- Scripts/levelButton.gd
+++ Scripts/levelButton.gd
@@ -1,12 +1,15 @@
 extends Button;
 
 @export var levelNumber : int;
-#
+@export var randomButtonBackground : Array[Texture2D];
+@onready var back: TextureRect = $TextureRect
+
 func _ready():
 	if levelNumber:
-		text = "LEVEL%s" % levelNumber;
-	#text = "test"
-
+		text = "LEVEL %s" % levelNumber;
+	$TextureRect.texture = randomButtonBackground[randi_range(0,randomButtonBackground.size()-1)];
+	if randi_range(0,1)== 1: back.flip_h = true;
+	if randi_range(0,1)== 1: back.flip_v = true;
 func _on_pressed():
 	if levelNumber:
 		SignalBus.levelChange.emit(levelNumber);
