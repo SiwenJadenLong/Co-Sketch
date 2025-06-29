34eba93b8601b1272c75c8b9bfd3cdacc4a2821f
Kayla Blosser <kayla.blosser@proton.me>
2025-06-29 18:29:53 -0400

    Add coins to top bar

--- Scripts/stats.gd
+++ Scripts/stats.gd
@@ -4,6 +4,9 @@ extends Control;
 @onready var stopwatch: Label = $MarginContainer/HBoxContainer/Stopwatch;
 @onready var current_level: Label = $MarginContainer/HBoxContainer/currentLevel;
 
+@onready var coinCounter = $MarginContainer/HBoxContainer/VBoxContainer/coin/Label;
+@onready var pennyCounter = $MarginContainer/HBoxContainer/VBoxContainer/penny/Label;
+
 #connect levelwon signal to hiding the stats
 func _ready():
 	SignalBus.levelWon.connect(_levelWon)
@@ -14,6 +17,8 @@ func _process(_delta):
 	inkUsed.value = GlobalVariables.inkLimit-GlobalVariables.totalLineDistance;
 	stopwatch.text = str(GlobalVariables.levelTime).pad_decimals(2);
 	current_level.text = "Level %s" % GlobalVariables.currentLevelNumber;
+	coinCounter.text = str(GlobalVariables.coins);
+	pennyCounter.text = str(GlobalVariables.winConditionCoins);
 
 func _levelWon():
 	hide();
